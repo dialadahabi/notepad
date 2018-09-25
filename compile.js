8717cb10fbb315d67d11289e30a4df2eab53b7b3
dialadahabi <dahabidiala@hotmail.com>
2018-09-25 20:05:47 +0300

    Add tests for contract

--- compile.js
+++ compile.js
@@ -1,6 +1,7 @@
-const solc = require('solc');
 const path = require('path');
 const fs = require('fs');
-const notepadPath = path.resolve(__dirname, 'contracts', 'notepad.sol');
-const source = fs.readFileSync(notepadPath, 'utf8');
-module.exports = solc.compile(source,1).contracts[':notepad'];
+const solc = require('solc');
+const npPath = path.resolve(__dirname, 'contracts', 'notepad.sol');
+const source = fs.readFileSync(npPath, 'utf8');
+
+module.exports = solc.compile(source, 1).contracts[':Notepad'];
--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,4 +1,64 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); //constructor function
-const web3 = new Web3(ganache.provider()) //instance
+const web3 = new Web3(ganache.provider()); //instance
+const { interface, bytecode } = require('../compile');
+
+let fetchedAccounts;
+let notepad;
+let updatedNote;
+const INITIAL_NOTE = 'Test'
+
+
+beforeEach(async() => {
+	// List of all accounts
+	fetchedAccounts = await web3.eth.getAccounts();
+
+	// Use an account to deploy contract
+	notepad = await new web3.eth.Contract(JSON.parse(interface)).deploy({data: bytecode, arguments: [INITIAL_NOTE]}).send({from: fetchedAccounts[0], gas: '1000000'});
+});
+
+
+describe('notepad', () => {
+	it('deploys contract', () =>{
+		//console.log(notepad);
+		assert.ok(notepad.options.address);
+	});
+
+	it('has default note', async() =>{
+		const note = await notepad.methods.note().call();
+		assert.equal(note, INITIAL_NOTE);
+	});
+
+	it('can modify note', async()=>{
+		updatedNote = 'Diala'
+		await notepad.methods.setNote(updatedNote).send({from: fetchedAccounts[0]});
+		const note = await notepad.methods.note().call();
+		assert.equal(note, updatedNote);
+	})
+});
+
+// class Animal {
+// 	eat(){
+// 		return 'Eating';
+// 	}
+// 	walk(){
+// 		return 'walking';
+// 	}
+// }
+
+// let animal;
+
+// beforeEach(() => {
+// 	animal = new Animal();
+// });
+
+// describe('Animal Class', () =>{
+// 	it('should return eating', () => {
+// 		assert.equal(animal.eat(), 'Eating');
+// 	});
+// 	it('should return walking', () => {
+// 		assert.equal(animal.walk(), 'walking');
+// 	});
+// });
+
